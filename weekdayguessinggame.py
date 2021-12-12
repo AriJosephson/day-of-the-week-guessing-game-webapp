7a7a857714445d54ab468f4ea00ad4e3b9a4ccf6
arijosephson <ajosephson98@yahoo.com>
2021-12-12 17:34:23 +0100

    Updated Python file

--- weekdayguessinggame.py
+++ weekdayguessinggame.py
@@ -10,7 +10,7 @@ import random
 from flask import Flask, request, render_template
 
 def generate_date(cutoff=80, lang='en_US', date_format='%Y-%m-%d'):
-    locale.setlocale(locale.LC_ALL, lang)
+    #locale.setlocale(locale.LC_ALL, lang)
     randunivar = random.uniform(0, 1) # get uniform random variable from 0 to 1
     
     if randunivar < cutoff/100: # cutoff% of the time do dates within a few centuries of the present
@@ -33,10 +33,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template("weekday_guesser.html",
-                           cutoff=80,
-                           language="en_US",
-                           date_format="%Y-%m-%d")
+    return render_template("weekday_guesser.html", cutoff=80)
 
 @app.route('/', methods=['POST'])
 def my_form_post():
